d806605a40c16d45a3adb4644d114cb1fb609d5e
Michael Pascuzzi <gatorpazz@gmail.com>
2017-08-31 20:53:01 -0400

    Finish application

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,7 +9,7 @@ class ListItem extends Component {
     this.onRowPress = this.onRowPress.bind(this);
   }
   onRowPress() {
-    Actions.employeeCreate({ employee: this.props.employee });
+    Actions.employeeEdit({ employee: this.props.employee });
   }
   render() {
     const { name } = this.props.employee;
